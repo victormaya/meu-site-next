5547f11aecd48c54a0d77e8b775232aa656c8a12
Victor Maya <victor.maya42@gmail.com>
2022-05-14 14:34:53 -0300

    deploy

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Head from '../Head';
 import { ContainerLoading } from './styled';
 import { ContainerDefaultPage } from '../../styles/ContainerPagesStyled';
 
 function Loading() {
   return (
     <ContainerDefaultPage>
+      <Head title="..." />
       <ContainerLoading>
         <div className="circle" />
       </ContainerLoading>
