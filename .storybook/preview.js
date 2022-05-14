5e78a2a1e145c13acb7f2d619735af52e869b948
Victor Maya <victor.maya42@gmail.com>
2022-05-14 10:40:23 -0300

    configuração inicial

---
+++ .storybook/preview.js
@@ -0,0 +1,40 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../src/styles/global-styles'
+import { theme } from '../src/styles/theme'
+import * as NextImage from 'next/image'
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage {...props} unoptimized loader={({ src }) => src} />
+  ),
+})
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ]
+  }
+}
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+];
