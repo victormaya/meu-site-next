cdc6c4d4c8b1e614700f219ef172d143047b077f
Victor Maya <victor.maya42@gmail.com>
2022-10-15 09:18:30 -0300

    fix: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,51 @@
 import React from 'react'
 
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import { ContainerHeader } from './styled'
 
 function Header() {
+  const { asPath } = useRouter()
+
   return (
     <ContainerHeader>
       <nav>
-        <Link href="/skills">Skills</Link> |
-        <Link href="/experiencia">Experiência</Link> |
-        <Link href="/portifolio">Portifólio</Link> |
-        <Link href="/contato">Contato</Link>
+        <Link href="/skills">
+          <a href="skills" className={asPath === '/skills' ? 'active' : ''}>
+            Skills
+          </a>
+        </Link>{' '}
+        |
+        <Link href="/experiencia">
+          <a
+            href="experiencia"
+            className={asPath === '/experiencia' ? 'active' : ''}
+          >
+            {' '}
+            Experiência
+          </a>
+        </Link>{' '}
+        |
+        <Link
+          href="/portifolio"
+          className={asPath === '/portifolio' ? 'active' : ''}
+        >
+          <a
+            href="portifolio"
+            className={asPath === '/portifolio' ? 'active' : ''}
+          >
+            {' '}
+            Portifólio
+          </a>
+        </Link>{' '}
+        |
+        <Link href="/contato" className={asPath === '/contato' ? 'active' : ''}>
+          <a href="contato" className={asPath === '/contato' ? 'active' : ''}>
+            {' '}
+            Contato
+          </a>
+        </Link>
       </nav>
     </ContainerHeader>
   )
