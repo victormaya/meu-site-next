3e4d31e791b4becd174f75bb11c5ed723764ee08
Victor Maya <victor.maya42@gmail.com>
2022-10-17 12:32:16 -0300

    feat/ change loading

--- src/components/Loading/styled.tsx
+++ src/components/Loading/styled.tsx
@@ -6,17 +6,61 @@ export const ContainerLoading = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  .circle {
-    width: 80px;
-    height: 80px;
+  .loader,
+  .loader:before,
+  .loader:after {
     border-radius: 50%;
-    border: solid 10px ${({ theme }) => theme.colors.orangeText};
-    border-top-color: #2b2b2b;
-    animation: rotate 1s infinite;
-    @keyframes rotate {
-      to {
-        transform: rotate(1turn);
-      }
+    width: 2.5em;
+    height: 2.5em;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    -webkit-animation: load7 1.8s infinite ease-in-out;
+    animation: load7 1.8s infinite ease-in-out;
+  }
+  .loader {
+    color: ${({ theme }) => theme.colors.orangeText};
+    font-size: 10px;
+    margin: 80px auto;
+    position: relative;
+    text-indent: -9999em;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+  }
+  .loader:before,
+  .loader:after {
+    content: '';
+    position: absolute;
+    top: 0;
+  }
+  .loader:before {
+    left: -3.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+  .loader:after {
+    left: 3.5em;
+  }
+  @-webkit-keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 0;
+    }
+  }
+  @keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 0;
     }
   }
 `
