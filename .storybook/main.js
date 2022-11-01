1cdf7ab74c52c338c976d7c91573cc7d988082be
Victor Maya <victor.maya42@gmail.com>
2022-11-01 17:16:46 -0300

    sb

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../src/components/**/stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    'storybook-addon-next'
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: "webpack5"
+  }
+}
