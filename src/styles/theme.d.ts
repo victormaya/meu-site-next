1f3a427034d67a312ce0d423cd4cf16795e32d58
Victor Maya Nascimento Almeida <victor.maya42@gmail.com>
2025-09-12 10:11:03 -0300

    fix: node

---
+++ src/styles/theme.d.ts
@@ -0,0 +1,25 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      background: string
+      linearGradient: string
+      greyText: string
+      orangeText: string
+      greyBg: string
+    }
+    font: {
+      family: {
+        default: string
+        custom: string
+      }
+      sizes: {
+        xsmall: string
+        small: string
+        medium: string
+        large: string
+      }
+    }
+  }
+}
